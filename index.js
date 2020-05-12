a82291c42e0205bddf032c315bc617010368f01b
Md Mehedi Hasan <me.ovee@gmail.com>
2020-05-12 17:20:23 +0600

    Stable commit along few functionalities

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 //Md Mehedi Hasan
 
-//question ids on HTML
+//question ids on HTML h3 tag
 const idQ1 = "Q1";
 const idQ2 = "Q2";
 const idQ3 = "Q3";
 const idQ4 = "Q4";
 
 //constructor for questions
-function Question(no, title, type, options) {
+function Question(no, title, type) {
   this.no = no;
   this.title = title;
   this.type = type;
-  this.options = options;
 }
 
 //constructor for answers
@@ -32,26 +31,65 @@ const ques2 = new Question(
   "MCQ"
 );
 
-const ques3 = new Question(3, "What skills do you have?", "MRQ");
+const ques3 = new Question(
+  3,
+  "What are the following skills do you have?",
+  "MRQ"
+);
 
-const ques4 = new Question(4, "Shortly describe abour yourself.", "ESSAY");
+const ques4 = new Question(
+  4,
+  "Shortly describe about your current role.",
+  "ESSAY"
+);
 
 //manipulating dom
 document.getElementById(idQ1).innerHTML = idQ1 + ". " + ques1.title;
 document.getElementById(idQ2).innerHTML = idQ2 + ". " + ques2.title;
+document.getElementById(idQ3).innerHTML = idQ3 + ". " + ques3.title;
+document.getElementById(idQ4).innerHTML = idQ4 + ". " + ques4.title;
 
-var answer = document.getElementById("1").value;
-
-function fieldValidator() {
-  var answer = document.getElementById("1").value;
-  if (answer == "") {
-    console.log("one of them is not null");
+//Selecting all the elements on select all click for question3
+document.getElementById("select-all").onclick = function () {
+  var checkboxes = document.getElementsByName("q3answer");
+  for (var checkbox of checkboxes) {
+    checkbox.checked = this.checked;
   }
-  var ele = document.getElementsByName("q1answer");
+};
 
+//fetching all the answers
+function fetchAnswers() {
+  var answer1 = document.getElementById("q1answer").value;
+  var answer2 = "";
+  var answer3 = "";
+  var answer4 = document.getElementById("q4answer").value;
+
+  //fetching Q2 answers
+  var ele = document.getElementsByName("q2answer");
   for (i = 0; i < ele.length; i++) {
-    if (ele[i].checked) console.log(ele[i].value);
+    if (ele[i].checked) {
+      answer2 = ele[i].value;
+    }
+  }
+
+  //fetching Q3 answers
+  var items = document.getElementsByName("q3answer");
+  var answer3 = "";
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type == "checkbox" && items[i].checked == true)
+      answer3 += items[i].value + "\n";
+  }
+  //console.log(answer1, answer2, answer3, answer4);
+  fieldValidator(answer1, answer2, answer4);
+}
+
+//validating if all required answers were given
+function fieldValidator(answer1, answer2, answer4) {
+  if (answer1 == "" || answer2 == "" || answer4 == "") {
+    alert("Answering Q1,Q2,Q3 are mandatory!");
   }
 }
 
-console.log(ques1, ques2, ques3, ques4);
+function answerInsert() {}
+
+//console.log(ques1, ques2, ques3, ques4);
